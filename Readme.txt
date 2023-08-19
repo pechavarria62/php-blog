7b64a4437cf0e27dba410fd595717de733b6ff3f
Pedro Echavarria <pechavarria62@gmail.com>
2023-08-19 07:09:28 -0500

    added a screenshot and connected the style file to the page

--- Readme.txt
+++
@@ -1,2 +0,0 @@
-stopped video at 23:53 
-https://www.youtube.com/watch?v=-h7gOJbIpmo&t=1176s
--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
     
     function registered_styles(){
-        
+        wp_enqueue_style('bootstrap',get_template_directory_uri() . 'theme-template/css/style.css', array(), '1.0', 'all');
     }
-
+    
+    add_action('wp_enqueue_scripts', 'registered_styles');
 ?>
