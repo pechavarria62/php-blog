b8b7c5848312cfdef66b2d1208069f7f2ecb7017
Pedro Echavarria <pechavarria62@gmail.com>
2023-08-19 22:48:38 -0500

    renamed my templates folder, still working on making my css work don't know cus for a guy it works even tho the style file doesn't have anything on it and the style file that has everything on it is in another forlder

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
     function registered_styles(){
-        wp_enqueue_style('bootstrapito', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+        wp_enqueue_style('php-blog-css', get_template_directory_uri() . "/style.css", array('php-blog-bootstrap'), '1.0', 'all');
+        wp_enqueue_style('php-blog-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", array(), '1.0', 'all');
+        wp_enqueue_style('php-blog-fontawesome ',"https://kit.fontawesome.com/a329bb9f5d.js", array(), '1.0', 'all');
     }
     add_action('wp_enqueue_scripts', 'registered_styles');
 ?>
