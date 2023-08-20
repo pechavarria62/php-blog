62e2130d10236600371d906a7260aeb9278f8867
Pedro Echavarria <pechavarria62@gmail.com>
2023-08-19 21:49:50 -0500

    working on making css to work

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
-    <meta name="author" content="https://youtube.com/FollowAndrew">    
+    <!-- <meta name="author" content="https://youtube.com/FollowAndrew">    -->
     <link rel="shortcut icon" href="images/logo.png"> 
     
     <!-- FontAwesome CSS-->
@@ -16,10 +16,9 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
 	<!-- Theme CSS -->
 	<?php
-		
 		wp_head();
 	?>
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
+	
 
 </head> 
 
--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php
-    
     function registered_styles(){
-        wp_enqueue_style('bootstrap',get_template_directory_uri() . 'theme-template/css/style.css', array(), '1.0', 'all');
+        wp_enqueue_style('bootstrapito', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
     }
-    
     add_action('wp_enqueue_scripts', 'registered_styles');
 ?>
