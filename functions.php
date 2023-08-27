5c2be1cac832da0dc11022ef594a4614db471640
Pedro Echavarria <pechavarria62@gmail.com>
2023-08-27 18:56:56 -0500

    added a content-article file, stated displaying contect in the single.php file

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
     function themeTagSupport(){
         add_theme_support('title-tag');
         add_theme_support('custom-logo');
+        add_theme_support('post-thumbnails');
     }
     add_action('after_setup_theme','themeTagSupport');
     
--- header.php
+++ header.php
@@ -15,49 +15,35 @@
 	</head> 
 
 	<body>
-		
 		<header class="header text-center">	    
-			<a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
-			
+			<a class="site-title pt-lg-4 mb-0" href="index.html">
+				<?php
+					echo get_bloginfo('name');  
+				?>
+			</a>
 			<nav class="navbar navbar-expand-lg navbar-dark" >
-				
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 				</button>
-
 				<div id="navigation" class="collapse navbar-collapse flex-column" >
-					<img class="mb-3 mx-auto logo" src="/wp-content/themes/php-blog/assets/images/logo.png" alt="logo" >	
 					<?php
-					
+						if (function_exists('the_custom_logo')){
+							$custom_logo_id = get_theme_mod('custom_logo');
+							$logo = wp_get_attachment_image_src($custom_logo_id);
+							// the_custom_logo();
+						}
+					?>
+					<img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo" >	
+					<?php
 						wp_nav_menu(
 							array(
 								'menu'=>'primary',
 								'container'=>'',
 								'theme_location'=>'primary',
 								'items_wrap'=>'<ul id="" class="navbar-nav flex-column text-sm-center text-md-left" >%3$s</ul>'
-
 							)
 						);
-					
 					?>
-					
-					<!-- <ul class="navbar-nav flex-column text-sm-center text-md-left">
-						<li class="nav-item active">
-							<a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="post.html"><i class="fas fa-file-alt fa-fw mr-2"></i>Blog Post</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="page.html"><i class="fas fa-file-image fa-fw mr-2"></i>Blog Page</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="archive.html"><i class="fas fa-archive fa-fw mr-2"></i>Blog Archive</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw mr-2 "></i>Contact Us</a>
-						</li>
-					</ul> -->
 					<hr>
 					<ul class="social-list list-inline py-3 mx-auto">
 						<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
